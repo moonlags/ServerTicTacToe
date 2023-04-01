43e7fc0d2ce1cca63912cb2db68539b3db32da04
moonlags <moonlags.me@gmail.com>
2023-04-01 20:29:06 +0300

    players

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,29 @@
 #include "Game.h"
 
+Game::Game(uint32_t gameIdm, string player): gameId(gameIdm), playersLen(1)
+{   
+    memset(&players, 0, 2);
+    addPlayer(player);
+    memset(&field, 0, 9);
+}
+
+Game::~Game()
+{
+    memset(&players, 0, 2);
+    memset(&field, 0, 9);
+}
+
+uint32_t Game::getGameId()
+{
+    return gameId;
+}
+
+void Game::addPlayer(string player)
+{
+    players[playersLen] = player;
+    playersLen++;
+}
+
 char Game::getField(int x)
 {
     return field[x];
@@ -7,7 +31,7 @@ char Game::getField(int x)
 
 bool Game::setField(int x, char value)
 {
-    if (field[x] ==0){
+    if (!field[x]){
         field[x] = value;
         return true;
     }
--- Game.h
+++ Game.h
@@ -1,14 +1,26 @@
 #pragma once
-#include <cstdint>
+
+#include <iostream>
+
+using namespace std;
 
 class Game
 {
 public:
+	Game(uint32_t gameId,string player);
+	~Game();
+
+	uint32_t getGameId();
+
+	void addPlayer(string player);
+
 	char getField(int x);
 	bool setField(int x, char value);
 	char checkWin();
 private:
 	uint32_t gameId;
-	char field[9] = {};
+	char field[9];
+	string players[2];
+	int playersLen;
 };
 
